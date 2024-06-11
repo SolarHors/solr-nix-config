75ad03a96cd7495d31f5b21aa4d3ec4c5e5f0e12
orange-hors <orange.hors@proton.me>
2024-06-11 18:01:09 +0300

    Change steam.nix to games.nix and clean up

--- user-modules/games.nix
+++ user-modules/games.nix
@@ -1,5 +1,4 @@
-# Steam video game digital distribution service configuration
-# (https://store.steampowered.com/)
+# Configuration for enabling gaming
 
 { config, pkgs, ... }:
 
@@ -13,7 +12,14 @@
       # Open ports in the firewall for Source Dedicated Server
       dedicatedServer.openFirewall = true;
     };
+    # TODO: Add proper gamescope config
     # Enable Gamescope microcompositor
     gamescope.enable = true;
   };
+
+  home.packages = with pkgs; [
+    gamescope    # Micro-compositor tailored towards gaming
+    mangohud     # Overlay for monitoring game statistics
+    bottles      # An easy-to-use wineprefix manager
+  ]
 }
