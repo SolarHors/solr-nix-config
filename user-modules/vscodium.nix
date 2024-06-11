37e123b25f5d8f07992e39455b1fba46a9b2cbec
orange-hors <orange.hors@proton.me>
2024-06-11 19:13:18 +0300

    Add several VSCodium extensions

--- user-modules/vscodium.nix
+++ user-modules/vscodium.nix
@@ -7,13 +7,15 @@
     # Enable VSCodium
     enable = true;
     package = pkgs.vscodium;
-    # Install extensions
+    # Install extensions by their ID
     extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide            # Nix support
       ms-python.python              # Python support
       ms-python.black-formatter     # Python formatter
       ms-pyright.pyright            # Python LSP
       rust-lang.rust-analyzer       # Rust LSP
       vadimcn.vscode-lldb           # LLDB debugger
+      bungcip.better-toml           # Better TOML support
       llvm-vs-code-extensions.vscode-clangd  # C/C++ LSP
     ];
     # Configure user settings
