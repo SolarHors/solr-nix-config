79573b01f7f31814c1bd92bbd67297aee7c6dc19
orange-hors <orange.hors@proton.me>
2024-06-14 01:32:53 +0300

    Remove empty hardware-configuration.nix file

--- system-profiles/orange-desktop/hardware-configuration.nix
+++
@@ -1,6 +0,0 @@
-#
-# Replace with your generated hardware-configuration.nix file
-# or use the command below to generate one (make sure your system is formatted)
-#
-# $ nixos-generate-config --show-hardware-config > ./hardware-configuration.nix
-#
