8b35ee6c70b74be1755e07c3093eb53659d46d2a
orange-hors <orange.hors@proton.me>
2024-06-18 01:07:49 +0300

    Update HM games config

--- user-modules/games.nix
+++ user-modules/games.nix
@@ -4,19 +4,15 @@
 
 {
   home.packages = with pkgs; [
+    gamescope         # Micro-compositor tailored towards gaming
+    mangohud          # Overlay for monitoring game statistics
+    bottles           # An easy-to-use wineprefix manager
+    steam             # Digital distribution platform
+    # Compatibility packages (probably) required by Steam
+    libpulseaudio
     xorg.libXcursor
     xorg.libXi
     xorg.libXinerama
     xorg.libXScrnSaver
-    libpng
-    libpulseaudio
-    libvorbis
-    stdenv.cc.cc.lib
-    libkrb5
-    keyutils
-    gamescope
-    mangohud
-    bottles
-    steam
   ];
 }
