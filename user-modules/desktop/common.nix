042bdab05d1b2f930cbe41c36168c436c122b8f6
orange-hors <orange.hors@proton.me>
2024-07-04 16:54:10 +0300

    Undo Qt unification to fix DE loading issues

--- user-modules/desktop/common.nix
+++ user-modules/desktop/common.nix
@@ -3,19 +3,20 @@
 { config, pkgs, ... }:
 
 {
-  # Unift GTK3 and GTK4 application styles
-  gtk = {
-    enable = true;
-    theme = {
-      name = "adw-gtk3";
-      package = pkgs.adw-gtk3;
-    };
-  };
+  # # Unify GTK3 and GTK4 application styles
+  # gtk = {
+  #   enable = true;
+  #   theme = {
+  #     name = "adw-gtk3";
+  #     # Package is defined by Stylix
+  #     # package = pkgs.adw-gtk3;
+  #   };
+  # };
 
-  # Unify Qt and GTK applications
-  qt = {
-    enable = true;
-    platformTheme = "gtk2";
-    style = "adwaita-dark";
-  };
+  # # Unify Qt and GTK applications
+  # qt = {
+  #   enable = true;
+  #   platformTheme.name = "gtk";
+  #   style.name = "adwaita-dark";
+  # };
 }
